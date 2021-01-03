b59c78f37aaadc7667e6f004f366d5674b01412d
tankplanker <tankp@planker.com>
2021-01-03 10:10:59 +0000

    userspace first successful compile

--- keyboards/thevankeyboards/v4n4g0n/keymaps/tankplanker/config.h
+++ keyboards/thevankeyboards/v4n4g0n/keymaps/tankplanker/config.h
@@ -1,8 +1,3 @@
 #pragma once
 
-// place overrides here
-#define PERMISSIVE_HOLD
-#define TAPPING_TERM 180
-#define IGNORE_MOD_TAP_INTERRUPT
-//#define TAP_CODE_DELAY 5
-//#define TAPPING_FORCE_HOLD
+
